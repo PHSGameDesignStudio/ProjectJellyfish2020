1992ceb32a79e2d5f176332891fbde02e7af72fd
SkyanSam <samuelwiggins15@gmail.com>
2020-03-04 17:56:34 -0500

    init 2018.4.12f1

---
+++ JellyfishGame/Assets/Scripts/Player/CharScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharScript : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = speed;
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        // Move translation along the object's z-axis
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0,translation,0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-translation,0,0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(translation,0,0);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0,-translation,0);
+        }
+    }
+}
---
+++ JellyfishGame/Assets/Scripts/Player/InventoryScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    [SerializeField] private Component m_Box1;
+    [SerializeField] private Component m_Box2;
+    [SerializeField] private Component m_Box3;
+    [SerializeField] private Component m_Box4;
+    [SerializeField] private Component m_Box5;
+    [SerializeField] private Component m_Box6;
+    [SerializeField] private Component m_Box7;
+    [SerializeField] private Component m_Box8;
+    private Texture2D[] Images = new Texture2D[]{null,null,null,null,null,null,null,null};
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void AddItem()
+    {
+        
+    }
+}
