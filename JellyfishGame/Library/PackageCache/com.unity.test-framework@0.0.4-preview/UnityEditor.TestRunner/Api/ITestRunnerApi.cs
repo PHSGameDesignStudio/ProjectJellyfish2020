806b32f62c778c74b5cbd9bec442bd1f9a73cfc8
SkyanSam <samuelwiggins15@gmail.com>
2020-03-08 10:09:56 -0400

    .meta sprite

--- JellyfishGame/Library/PackageCache/com.unity.test-framework@0.0.4-preview/UnityEditor.TestRunner/Api/ITestRunnerApi.cs
+++
@@ -1,8 +0,0 @@
-namespace UnityEditor.TestTools.TestRunner.Api
-{
-    interface ITestRunnerApi
-    {
-        void Execute(ExecutionSettings executionSettings = null);
-        void RegisterCallbacks<T>(T testCallbacks) where T : ICallbacks;
-    }
-}
--- JellyfishGame/Library/PackageCache/com.unity.test-framework@0.0.4-preview/UnityEditor.TestRunner/Api/TestStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace UnityEditor.TestTools.TestRunner.Api
-{
-    internal enum TestStatus
-    {
-        Skipped,
-        Passed,
-        Failed,
-    }
-}
