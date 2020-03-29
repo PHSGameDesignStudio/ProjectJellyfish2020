fec0bcb02bc2cc8ef29b439e5407de7dc442fb7e
Miggy-Chan <miguelventura1123@gmail.com>
2020-03-28 21:55:27 -0400

    Inventory, But Better
    - The use button works for Consumables
    - The drop function acts as a total discard
    - The Examine button works for items with a description
    - You may now select your items, with sprites that show up nicely

--- JellyfishGame/Assets/Scripts/Player/InventoryScript.cs
+++ JellyfishGame/Assets/Scripts/Player/InventoryScript.cs
@@ -6,16 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class InventoryScript : MonoBehaviour
 {
-    public Component m_Box1;
-    public Component m_Box2;
-    public Component m_Box3;
-    public Component m_Box4;
-    public Component m_Box5;
-    public Component m_Box6;
-    public Component m_Box7;
-    public Component m_Box8;
-    
-    
     void Start()
     {
         GameObject.Find("Dropdown").SetActive(false);
