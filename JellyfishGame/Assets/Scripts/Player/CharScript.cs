e967e741c2585dabc15460f8b82dd9ea1970626d
Miggy-Chan <miguelventura1123@gmail.com>
2020-04-01 19:54:12 -0400

    Animation
    Stel moves now...

--- JellyfishGame/Assets/Scripts/Player/CharScript.cs
+++ JellyfishGame/Assets/Scripts/Player/CharScript.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class CharScript : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
+    public bool facingRight = true;
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
@@ -15,19 +16,41 @@ public class CharScript : MonoBehaviour
         // Move translation along the object's z-axis
         if (Input.GetKey("w"))
         {
-            transform.Translate(0,translation,0);
+            transform.Translate(0, translation, 0);
+            animator.SetBool("Backwards", true);
         }
+        else { animator.SetBool("Backwards", false); }
         if (Input.GetKey("a"))
         {
-            transform.Translate(-translation,0,0);
+            animator.SetBool("Walking", true);
+            transform.Translate(-translation, 0, 0);
+            if (!facingRight)
+            {
+                Flip();
+            }
         }
-        if (Input.GetKey("d"))
+        else if (Input.GetKey("d"))
         {
-            transform.Translate(translation,0,0);
+            animator.SetBool("Walking", true);
+            transform.Translate(translation, 0, 0);
+            if (facingRight)
+            {
+                Flip();
+            }
         }
+        else { animator.SetBool("Walking", false); }
         if (Input.GetKey("s"))
         {
-            transform.Translate(0,-translation,0);
+            animator.SetBool("Forward", true);
+            transform.Translate(0, -translation, 0);
         }
+        else{animator.SetBool("Forward", false); }
+    }
+    public void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
