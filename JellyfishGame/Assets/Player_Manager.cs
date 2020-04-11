76f95b9689ea2a3c13e915a82dbf9a39c7ea00e6
Miggy-Chan <miguelventura1123@gmail.com>
2020-04-11 12:26:10 -0400

    Overworld Sample
    Layers have been added to the overworld

---
+++ JellyfishGame/Assets/Player_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Manager : MonoBehaviour
+{
+    public int HPLevel;
+    public int XPLevel;
+    public string charName;
+    public CharScript Inventory;
+    public Slider HPSlider;
+    public Slider XPSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
