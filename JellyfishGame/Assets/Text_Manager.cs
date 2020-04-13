df9cac808d029627c72a1d7db23164a6a9038caf
Miggy-Chan <miguelventura1123@gmail.com>
2020-04-13 15:27:01 -0400

    Better Dialouge
    It is now automatic

--- JellyfishGame/Assets/Text_Manager.cs
+++ JellyfishGame/Assets/Text_Manager.cs
@@ -8,7 +8,9 @@ public class Text_Manager : MonoBehaviour
     public GameObject TextBox;
     Vector3 theWidth;
     public GameObject[] textDialougeList = new GameObject[] { };
+    public GameObject[] textDialougeTriggers = new GameObject[] { };
     public int Dialouge = 0;
+    public GameObject Player;
     void Start()
     {
         TextBox.SetActive(true);
@@ -20,14 +22,15 @@ public class Text_Manager : MonoBehaviour
     IEnumerator TextboxAnimate()
     {
         theWidth = TextBox.transform.localScale;
-        for (int i = 0; i < (TextBoxWidth) + 1; i = i+25)
+        Player.GetComponent<CharScript>().enabled = false;
+        for (int i = 0; i < (TextBoxWidth) + 1; i = i + 25)
         {
             theWidth.x = i;
             TextBox.transform.localScale = theWidth;
             yield return new WaitForSeconds(.01f);
         }
         textDialougeList[Dialouge].SetActive(true);
-        
+
     }
     IEnumerator TextBoxAnimateOff()
     {
@@ -40,7 +43,7 @@ public class Text_Manager : MonoBehaviour
             yield return new WaitForSeconds(.01f);
         }
         TextBox.SetActive(false);
-        Dialouge += 1;
+        Player.GetComponent<CharScript>().enabled = true;
     }
     // Update is called once per frame
     public void Close()
@@ -50,5 +53,32 @@ public class Text_Manager : MonoBehaviour
     private void OnEnable()
     {
         Start();
+    }
+    private void Checkif()
+    {
+        for (int i = 0; i < textDialougeTriggers.Length+1; i = i + 2)
+        {
+            if (textDialougeTriggers[i] != null)
+            {
+                Transform triggerPosTL = textDialougeTriggers[i].GetComponent<Transform>();
+                Transform triggerPosBR = textDialougeTriggers[i + 1].GetComponent<Transform>();
+                Transform playerPos = Player.GetComponent<Transform>();
+                Debug.Log("yote");
+                if (playerPos.localPosition.x > triggerPosTL.localPosition.x && playerPos.localPosition.y < triggerPosTL.localPosition.y && playerPos.localPosition.x < triggerPosBR.localPosition.x && playerPos.localPosition.y > triggerPosBR.localPosition.y)
+                {
+                    Debug.Log("yeet");
+                    Destroy(textDialougeTriggers[i]);
+                    Destroy(textDialougeTriggers[i + 1]);
+                    textDialougeTriggers[i] = textDialougeTriggers[i + 1] = null;
+                    Dialouge = i / 2 + 1;
+                    Start();
+                }
+            }
+
+        }
+    }
+    private void Update()
+    {
+        Checkif();
     }
 }
