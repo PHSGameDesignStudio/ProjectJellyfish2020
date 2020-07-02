6661a86ce7e05fb8f54856e645a9524ae779ea79
Miggy-Chan <miguelventura1123@gmail.com>
2020-07-01 23:02:04 -0400

    Triggers & Movement & Update
    Update to 2019.1
    Trigger system in place
    Animation: Moving Backward

---
+++ JellyfishGame/Assets/DestroyTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTrigger : MonoBehaviour
+{
+    public int Event;
+    public Text_Manager textManager;
+    public Collider2D Itself;
+    public GameObject Player;
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+            Debug.Log("hello");
+            textManager.Dialouge = Event;
+            Debug.Log(Event);
+        textManager.enabled = false;
+        textManager.enabled = true;
+        Destroy(Itself);
+    }
+}
