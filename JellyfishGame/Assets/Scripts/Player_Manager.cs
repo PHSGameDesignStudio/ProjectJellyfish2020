235c841602c8efcee0ecc6a28fae216ac8ff281f
Miggy-Chan <miguelventura1123@gmail.com>
2020-07-30 11:32:57 -0400

    update
    worked on the first scene

---
+++ JellyfishGame/Assets/Scripts/Player_Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Manager : MonoBehaviour
+{
+    public int HPLevel;
+    public int XPLevel;
+    public string charName;
+    public int[] Inventory = new int[8] {0,0,0,0,0,0,0,0};
+    public Slider HPSlider;
+    public Slider XPSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
