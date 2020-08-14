225898bc5031753b73243522cb1d074c05934068
Miggy-Chan <miguelventura1123@gmail.com>
2020-08-14 12:31:55 -0400

    An Update, cause yes
    - Event manager is being worked on
    - First scene is panned out
    - Bunch of sprites were added
    - Scripts were edited for more efficiency
    - Sam folder has one more sam

---
+++ JellyfishGame/Assets/Event1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event1 : MonoBehaviour
+{
+    public Transform amon;
+    public Transform stel;
+    public EventManager eventManager;
+    public Text_Manager textManager;
+    public Transform newPosition;
+    public int atEnd = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("loookokok");
+        StartCoroutine(Setup(stel, 5));
+        
+        
+        //eventManager.StartFadeOut();
+        //eventManager.StartFadeIn();
+        //
+    }
+
+    private void Update()
+    {
+        if (atEnd == 1) { StartCoroutine(Setup(amon, 6)); }
+        else if (atEnd == 2) { StartCoroutine(Setup(stel, 7)); }
+        else if (atEnd == 3) { eventManager.Teleport(newPosition); }
+        else if (atEnd == 4) { DestroyObject(this); }
+    }
+    public void TurnOnNewMessage()
+    {
+        textManager.enabled = false;
+        textManager.enabled = true;
+    }
+    public IEnumerator Setup(Transform lookingObject, int dialouge)
+    {
+        eventManager.MoveCamera(lookingObject);
+        textManager.Dialouge = dialouge;
+        TurnOnNewMessage();
+        while (textManager.TextBox.activeSelf == true)
+        {
+            yield return null;
+        }
+        atEnd++;
+
+    }
+}
---
+++ JellyfishGame/Assets/Scripts/EventManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public Transform player;
+    public Text_Manager textManager;
+    public SpriteRenderer jumpCut;
+    public Cinemachine.CinemachineVirtualCamera Camera; 
+
+    public void Teleport(Transform newPos)
+    {
+        player.position = newPos.position;
+    }
+    IEnumerator FadeOut()
+    { 
+        for(int i = 0; i < 255; i++)
+        {
+            jumpCut.color = new Color(1f, 1f, 1f, i);
+            yield return new WaitForSeconds(.0078f);      
+        }
+        StopCoroutine(FadeOut());
+    }
+    IEnumerator FadeIn()
+    {
+        for (int i = 255; i > 0; i = i-1)
+        {
+            jumpCut.color = new Color(1f, 1f, 1f, i);
+            yield return new WaitForSeconds(.0078f);
+        }
+        StopCoroutine(FadeIn());
+    }
+    public void StartFadeIn()
+    {
+        StartCoroutine("FadeIn");
+    }
+    public void StartFadeOut()
+    {
+        StartCoroutine("FadeOut");
+    }
+    public void MoveCamera(Transform newPos)
+    {
+        Camera.Follow = newPos;
+    }
+}
